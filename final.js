4d99b1b36cb2b2aed8fea5793c00a0259b93f817
kinuko1221 <kinuko1221@net44-dhcp180.sfc.keio.ac.jp>
2016-07-12 16:59:36 +0900

    試し

--- final.js
+++ final.js
@@ -1,1 +1,17 @@
 alert ("ok");
+
+
+DirectionsRequest(){
+  origin: LatLng | String | google.maps.Place,
+  destination: LatLng | String | google.maps.Place,
+  travelMode: TravelMode,
+  transitOptions: TransitOptions,
+  drivingOptions: DrivingOptions,
+  unitSystem: UnitSystem,
+  waypoints[]: DirectionsWaypoint,
+  optimizeWaypoints: Boolean,
+  provideRouteAlternatives: Boolean,
+  avoidHighways: Boolean,
+  avoidTolls: Boolean,
+  region: String
+}
