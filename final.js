9eec5ab7854ab2acbfeef3518fb1e18881f14f10
shoheiverson <s15195so@sfc.keio.ac.jp>
2016-07-12 19:44:13 +0900

    テキスト　出発　目的　追加

--- final.js
+++ final.js
@@ -11,8 +11,57 @@ var mapOptions = {
 // [canvas]に、[mapOptions]の内容の、地図のインスタンス([map])を作成する
 var map = new google.maps.Map( canvas , mapOptions ) ;
 
+var x;
+var y;
+
 function decide(){
  alert ("ok")
 }
 var decideButton = document.querySelector("[data-role=decide]");
 decideButton.addEventListener("click", decide);
+
+function go(){
+	var a= Math.floor(Math.random()*6)+1;
+	if(a==1){
+		x="〒223-0062 神奈川県横浜市港北区日吉本町１丁目２２−４";
+	}
+	else if(a==2){
+		x="〒223-0062 神奈川県横浜市港北区日吉本町１丁目２３−２";
+	}
+	else if(a==3){
+		x="〒223-0061、神奈川県横浜市港北区日吉2丁目1-8";
+	}
+	else if(a==4){
+		x="神奈川県横浜市港北区日吉本町１丁目１９−２１、日吉スカイビル1F";
+	}
+	else if(a==5){
+		x="〒223-0062 神奈川県横浜市港北区日吉本町１丁目３、神奈川県横浜市港北区日吉本町1丁目3−17";
+	}
+	else{
+		x="〒223-0062 神奈川県横浜市港北区日吉本町１丁目２−４";
+	}
+	getstart();
+}
+var GoButton = document.querySelector("[data-role=go]");
+GoButton.addEventListener("click", go);
+
+function getstart(){
+	console.log("start");
+	y=document.getElementById("start").value;
+	
+	Route();
+}
+
+function Route(){
+	var request={
+  origin:x,
+  destination:y,	
+  travelMode:google.maps.DirectionsTravelMode.WALKING	
+};
+directionsService.route(request, function(response, status) {
+  if (status==google.maps.DirectionsStatus.OK) {
+    directionsDisplay.setDirections(response);
+  }
+});
+
+}
