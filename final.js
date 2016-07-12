2c24c9a321b9b0607ba527064dc3ed4c668bc73f
kinuko1221 <kinuko1221@net44-dhcp180.sfc.keio.ac.jp>
2016-07-12 19:29:15 +0900

    functionShuffleRestaurant

--- final.js
+++ final.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 alert ("ok");
-=======
-=======
->>>>>>> shoheiverson/master
-=======
->>>>>>> shoheiverson/master
 
 var canvas = document.getElementById( 'map-canvas' ) ;
 
@@ -19,38 +12,33 @@ var mapOptions = {
 // [canvas]に、[mapOptions]の内容の、地図のインスタンス([map])を作成する
 var map = new google.maps.Map( canvas , mapOptions ) ;
 
-<<<<<<< HEAD
 function play(){
-<<<<<<< HEAD
+
   alert ("ok");
 }
 var playButton = document.querySelector("#decide");
 playButton.addEventListener("click", play);
 
->>>>>>> shoheiverson/master
-=======
   alert ("ok")
-}
 var playButton = document.querySelector("#decide");
 playButton.addEventListener("click", play);
->>>>>>> shoheiverson/master
-=======
+
 function decide(){
  alert ("ok")
 }
+
 var decideButton = document.querySelector("[data-role=decide]");
 decideButton.addEventListener("click", ShuffleRestaurants);
->>>>>>> shoheiverson/master
 
-var = ResLatitude
-var = ResLongitude
+var ResLatitude;
+var ResLongitude;
 
 function ShuffleRestaurants() {
 	rand = Math.floor(Math.random()*5);
-	if (rand == 0) ResLatitude = "35.552884" ResLongitude = "139.646072" msg = "たつ吉"; 
-	if (rand == 1) ResLatitude = "35.554231" ResLongitude = "139.646933" msg = "とんかつ三田"; 
-	if (rand == 2) ResLatitude = "35.554058" ResLongitude = "139.645572" msg = "武蔵屋";
-	if (rand == 3) ResLatitude = "35.552986" ResLongitude = "139.646392" msg = "くりの木";
-	if (rand == 4) ResLatitude = "35.553007" ResLongitude = "139.645508" msg = "クークリ";
+	if (rand == 0) ResLatitude = "35.552884"; ResLongitude = "139.646072"; msg = "たつ吉"; 
+	if (rand == 1) ResLatitude = "35.554231"; ResLongitude = "139.646933"; msg = "とんかつ三田"; 
+	if (rand == 2) ResLatitude = "35.554058"; ResLongitude = "139.645572"; msg = "武蔵屋";
+	if (rand == 3) ResLatitude = "35.552986"; ResLongitude = "139.646392"; msg = "くりの木";
+	if (rand == 4) ResLatitude = "35.553007"; ResLongitude = "139.645508"; msg = "クークリ";
 	alert(msg);
 }
