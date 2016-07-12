87ef1eae48f45b59568f3ae32c9176ab8f85a375
kinuko1221 <kinuko1221@net44-dhcp180.sfc.keio.ac.jp>
2016-07-12 18:11:34 +0900

    削除

--- final.js
+++ final.js
@@ -1,17 +1,23 @@
+<<<<<<< HEAD
 alert ("ok");
+=======
 
+var canvas = document.getElementById( 'map-canvas' ) ;
 
-DirectionsRequest(){
-  origin: LatLng | String | google.maps.Place,
-  destination: LatLng | String | google.maps.Place,
-  travelMode: TravelMode,
-  transitOptions: TransitOptions,
-  drivingOptions: DrivingOptions,
-  unitSystem: UnitSystem,
-  waypoints[]: DirectionsWaypoint,
-  optimizeWaypoints: Boolean,
-  provideRouteAlternatives: Boolean,
-  avoidHighways: Boolean,
-  avoidTolls: Boolean,
-  region: String
+var latlng = new google.maps.LatLng( 35.792621 , 139.806513 );
+
+var mapOptions = {
+	zoom: 15 ,				
+	center: latlng ,		
+};
+
+// [canvas]に、[mapOptions]の内容の、地図のインスタンス([map])を作成する
+var map = new google.maps.Map( canvas , mapOptions ) ;
+
+function play(){
+  alert ("ok");
 }
+var playButton = document.querySelector("#decide");
+playButton.addEventListener("click", play);
+
+>>>>>>> shoheiverson/master
