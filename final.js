5da4808247f8210a0f14bf1f3669603ac7787bca
shoheiverson <s15195so@sfc.keio.ac.jp>
2016-07-12 17:45:19 +0900

    マップを出した

--- final.js
+++ final.js
@@ -1,1 +1,13 @@
-alert ("ok");
+
+var canvas = document.getElementById( 'map-canvas' ) ;
+
+var latlng = new google.maps.LatLng( 35.792621 , 139.806513 );
+
+var mapOptions = {
+	zoom: 15 ,				
+	center: latlng ,		
+};
+
+// [canvas]に、[mapOptions]の内容の、地図のインスタンス([map])を作成する
+var map = new google.maps.Map( canvas , mapOptions ) ;
+
