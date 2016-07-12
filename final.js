da573d38edb7cf4f657506ba7dc5e18ba2d7fec8
kinuko1221 <kinuko1221@net44-dhcp180.sfc.keio.ac.jp>
2016-07-12 19:40:24 +0900

    訂正

--- final.js
+++ final.js
@@ -34,11 +34,12 @@ var ResLatitude;
 var ResLongitude;
 
 function ShuffleRestaurants() {
-	rand = Math.floor(Math.random()*5);
+var rand = Math.round(Math.random () * 5) + 1
 	if (rand == 0) ResLatitude = "35.552884"; ResLongitude = "139.646072"; msg = "たつ吉"; 
 	if (rand == 1) ResLatitude = "35.554231"; ResLongitude = "139.646933"; msg = "とんかつ三田"; 
 	if (rand == 2) ResLatitude = "35.554058"; ResLongitude = "139.645572"; msg = "武蔵屋";
 	if (rand == 3) ResLatitude = "35.552986"; ResLongitude = "139.646392"; msg = "くりの木";
 	if (rand == 4) ResLatitude = "35.553007"; ResLongitude = "139.645508"; msg = "クークリ";
+  if (rand == 5) ResLatitude = "35.553908"; ResLongitude = "139.645867"; msg = "ベンダーベンダー";
 	alert(msg);
 }
