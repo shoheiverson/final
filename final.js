fc1e0edfd8740db11c8acf6803066f8c8ebbb991
kinuko1221 <kinuko1221@net44-dhcp180.sfc.keio.ac.jp>
2016-07-12 19:14:51 +0900

    fanctionShuffleRestaurant

--- final.js
+++ final.js
@@ -11,7 +11,6 @@ alert ("ok");
 var canvas = document.getElementById( 'map-canvas' ) ;
 
 var latlng = new google.maps.LatLng( 35.792621 , 139.806513 );
-
 var mapOptions = {
 	zoom: 15 ,				
 	center: latlng ,		
@@ -40,5 +39,18 @@ function decide(){
  alert ("ok")
 }
 var decideButton = document.querySelector("[data-role=decide]");
-decideButton.addEventListener("click", decide);
+decideButton.addEventListener("click", ShuffleRestaurants);
 >>>>>>> shoheiverson/master
+
+var = ResLatitude
+var = ResLongitude
+
+function ShuffleRestaurants() {
+	rand = Math.floor(Math.random()*5);
+	if (rand == 0) ResLatitude = "35.552884" ResLongitude = "139.646072" msg = "たつ吉"; 
+	if (rand == 1) ResLatitude = "35.554231" ResLongitude = "139.646933" msg = "とんかつ三田"; 
+	if (rand == 2) ResLatitude = "35.554058" ResLongitude = "139.645572" msg = "武蔵屋";
+	if (rand == 3) ResLatitude = "35.552986" ResLongitude = "139.646392" msg = "くりの木";
+	if (rand == 4) ResLatitude = "35.553007" ResLongitude = "139.645508" msg = "クークリ";
+	alert(msg);
+}
